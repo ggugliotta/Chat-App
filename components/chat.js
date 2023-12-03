dfd012d34a27fdd3a9dbcefc09da64e25ade022a
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-12-03 14:03:13 -0600

    added try catch function

--- components/chat.js
+++ components/chat.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import { KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native';
-import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 import { addDoc, collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Chat = ({ route, navigation, db}) => {
+const Chat = ({ route, navigation, db, isConnected}) => {
   // Extract parameters from navigation route
   const  { name, color, userID } = route.params;
  
@@ -12,29 +13,45 @@ const Chat = ({ route, navigation, db}) => {
 
   let unsubMessages; // unsubscribe function for Firestore listener
 
-
   useEffect(() => {
-    // Set the title of the chat screen to the name of the user
-    navigation.setOptions({ title: name });
-    // Create a query to listen to the messages collection in Firestore
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (docs) => {
-     let newMessages = [];
-     docs.forEach(doc => {
-      newMessages.push({
-       id: doc.id,
-       ...doc.data(),
-       createdAt: new Date(doc.data().createdAt.toMillis())
-     })
-   })
-   // Update the messages state with the new messages
-   setMessages(newMessages);
- })
- // Unsubscribe from the query when the component unmounts
- return () => {
-   if (unsubMessages) unsubMessages();
- }
-}, []);
+
+    if (isConnected === true) {
+
+      // Set the title of the chat screen to the name of the user
+      navigation.setOptions({ title: name });
+
+      // unregister current onSnapshot listener to avoid registering multiple listeners when
+     // useEffect code is re-executed.
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      // Create a query to listen to the messages collection in Firestore
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unsubMessages = onSnapshot(q,
+        async (documentSnapshot) => {
+          let newMessages = [];
+          documentSnapshot.forEach(doc => {
+            newMessages.push({id: doc.id,...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()) });
+        });
+        try {
+          await AsyncStorage.setItem("messages", JSON.stringify(newMessages));
+        } catch (error) {
+          console.log(error.message);
+        }
+        setLists(newMessages);
+      });
+  } else {
+      Alert.alert("Connection lost");
+      disableNetwork(db);
+      loadCachedMessages();
+    }
+ 
+  // Unsubscribe from the query when the component unmounts
+  return () => {
+     if (unsubMessages) unsubMessages();
+  }
+}, [isConnected]);
   
 // Handler to send new messages to Firestore
 const onSend =  (newMessages) => {
@@ -58,13 +75,21 @@ const onSend =  (newMessages) => {
     );
   };
 
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  }
+
   // Render Main Chat UI 
   return (
     <View style={[styles.container, { flex: 1, backgroundColor: color }]}>
        <GiftedChat
          messages ={messages}
          renderBubble={renderBubble}
-         onSend={(messages) => onSend(messages)}
+         renderInputToolbar ={renderInput}
+         renderActions={renderCustomActions}
+         renderCustomView={renderCustomView}
+         onSend={messages => onSend(messages)}
          user={{
           _id: userID,
           name,
@@ -75,7 +100,7 @@ const onSend =  (newMessages) => {
           accessibilityRole="text"
         />
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null   }
-    </View>
+    </View> 
   );
 };
 
